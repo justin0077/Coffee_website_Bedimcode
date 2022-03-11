c22ab34d709a50dd56df755cb26687de611bdaac
justin0077 <>
2022-03-11 14:01:48 +0100

    Finished project Bedimcode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,20 @@
 /*=============== LOADER ===============*/
+onload = () => {
+    const load = document.getElementById("load");
 
+    setTimeout(() => {
+        load.style.display = "none";
+    }, 2500);
+}
 
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
-      navToggle = document.getElementById("nav-toggle"),
-      navClose = document.getElementById("nav-close");
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
 /*=============== MENU SHOW ===============*/
 /* Validate if constant exist */
-if(navToggle) {
+if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add("show-menu");
     })
@@ -16,7 +22,7 @@ if(navToggle) {
 
 /*=============== MENU HIDDEN ===============*/
 /* Validate if constant exist */
-if(navClose) {
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove("show-menu");
     })
@@ -38,7 +44,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 function scrollHeader() {
     const header = document.getElementById("header");
     //When scroll is greater than 50 viewport height, add scroll-header class to header tag
-    if(this.scrollY >= 50) {
+    if (this.scrollY >= 50) {
         header.classList.add("scroll-header");
     } else {
         header.classList.remove("scroll-header");
@@ -59,10 +65,10 @@ let mixerProducts = mixitup('.products__content', {
 });
 
 
-/* Default filter products */ 
+/* Default filter products */
 mixerProducts.filter('.delacies')
 
-/* Link active products */ 
+/* Link active products */
 const linkProducts = document.querySelectorAll(".products__item");
 
 function activeProducts() {
@@ -73,7 +79,35 @@ function activeProducts() {
 linkProducts.forEach(l => l.addEventListener('click', activeProducts));
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
+
+    if (this.scrollY >= 350) {
+        scrollUp.classList.add("show-scroll");
+    } else {
+        scrollUp.classList.remove("show-scroll");
+    }
+}
+window.addEventListener("scroll", scrollUp);
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
+        } else {
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+        }
+    })
+}
+window.addEventListener("scroll", scrollActive);
